4718474867c57157cd34968a047b1f0d1ae60c96
Parth-302 <parthgediya6@gmail.com>
2024-09-28 09:59:52 +0530

    incriment decriment

---
+++ incriment-decrement.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num;
+	
+	
+	num = 100;
+	
+	//incriment
+	num++; //post incriment
+	
+	++num; //pre incriment
+	
+	printf("the num is %d",num);
+	
+	num--; //post incriment
+	
+	--num; //pre incriment
+	
+	printf("\nthe num is %d",num);
+	
+	return 0;
+}
