ad17bcb71ad4cfc7355c956bd8a519373db4a3e7
Parth-302 <parthgediya6@gmail.com>
2024-09-29 11:12:05 +0530

    print numbers

---
+++ print-number trail.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int n,p;
+	printf("please enter maximum number of value :-");
+	scanf("%d",&n);
+	
+	
+	for(p=1;p<n;p++){
+		printf("%d",p);
+	}
+	return 0;
+}
---
+++ print-numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int n,p,num;
+	printf("please enter maximum number :-");
+	scanf("%d",&num);
+	
+	for(p=1;p<=num;p++){
+		printf("%d\n",p);
+	}
+	
+	return 0;
+}
