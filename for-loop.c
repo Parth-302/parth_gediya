cd4d927c3733cc00d4c5528555ca9d474e1b8837
Parth-302 <parthgediya6@gmail.com>
2024-09-29 10:41:01 +0530

    loop

---
+++ for-loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	
+	int x;
+	
+	for(x=0;x<9;x++){
+		printf("Good Morning\n");
+	}
+	return 0;
+}
---
+++ lession-for-loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	
+	for(a=0;a<1;a++){
+		printf(" * ");
+	}
+	printf("\n * * ");
+	printf("\n * * * ");
+	printf("\n * * * * ");
+	printf("\n * * * * * ");
+	printf("\n * * * * * * ");
+	return 0;
+}
