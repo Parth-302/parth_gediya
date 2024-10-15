87765d7334568383d2dc7d7c1394e6b290a633f5
Parth-302 <parthgediya6@gmail.com>
2024-10-15 10:38:21 +0530

    array.c

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	// array 
+	int num[5]={1,22,333,4444,55555};
+	
+	int i;
+	
+	printf("array is %d",num[4]);
+	
+	for (i=0;i<5;i++){
+		printf("\n%d",num[i]);
+	}
+	return 0;
+	
+}
