3dcc05ae2404305e048e2f7817a7fb31f9b9a029
Parth-302 <parthgediya6@gmail.com>
2024-10-17 11:42:21 +0530

    array

---
+++ artray-even-number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	int i,n;
+	int arr[100];
+	
+	printf("please enter a number less than 100 :-");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++){
+		printf("array[%d]",i);
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++){
+		printf("\narray[%d]",arr[i]);
+	}
+	
+	for(i=0;i<n;i+=2){
+		printf("\n%d",i);
+	}
+	return 0;
+}
