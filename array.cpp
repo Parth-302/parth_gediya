bfac2d3f3ff5ad9235dd540d3abb29e08680b74f
Parth-302 <parthgediya6@gmail.com>
2025-01-05 10:14:55 +0530

    c++

---
+++ array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	string cars[5] ={"VOLVO","BMW","FORD","AUDI","SKODA"};
+	
+	cout<<cars[0]<<"\n";
+	
+	cout<<cars[1]<<"\n";
+	
+	cout<<cars[2]<<"\n";
+	
+	cout<<cars[3]<<"\n";
+	
+	cout<<cars[4];
+	
+	return 0;
+}
