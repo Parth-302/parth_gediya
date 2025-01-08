12a9e1234f2defdaa925b4ce3c1f03fb24e52111
Parth-302 <parthgediya6@gmail.com>
2025-01-08 10:41:37 +0530

    c++

---
+++ enum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+enum Level {
+  LOW,
+  MEDIUM,
+  HIGH
+}; 
+
+int main() {
+  enum Level myVar = MEDIUM;
+  cout << myVar;
+  return 0;
+}
+
---
+++ string_referance_function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void modifyStr(string &str) {
+  str += " World!";
+}
+
+int main() {
+  string greeting = "Hello";
+  modifyStr(greeting);
+  cout << greeting;
+  return 0;
+}
+
