41e38559925a46040507389e9f7b3a2fab16bfc1
Parth-302 <parthgediya6@gmail.com>
2025-01-10 15:03:04 +0530

    .c

---
+++ pattern_square.c
@@ -0,0 +1,11 @@
+#include  <stdio.h> 
+int i,j;
+int main() {
+    for ( i = 0; i  <  5; i++) {
+        for ( j = 0; j  <  5; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
