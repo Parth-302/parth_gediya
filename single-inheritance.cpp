aabacc1fc82da00a00d688aa2aa74abd017b5d2b
Parth-302 <parthgediya6@gmail.com>
2025-02-15 10:21:31 +0530

    c++

---
+++ single-inheritance.cpp
@@ -0,0 +1,39 @@
+//inheritance om cpp
+#include<iostream>
+
+using namespace std;
+
+
+//parent class
+class A{
+	
+	protected:
+		
+	void print(){
+		
+		cout<<"\nTHis is Parent Class";
+	}
+	
+};
+
+
+//child class
+
+class B : public A{
+	
+	public:
+	void out(){
+		
+		cout<<"This is Child CLass";
+		
+		print();
+	}
+};
+
+int main(){
+	
+	B a1;
+	
+	a1.out();
+	return 0;
+}
