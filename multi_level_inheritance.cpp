90d335af585006ae0bf532e8409635171464da8c
Parth-302 <parthgediya6@gmail.com>
2025-02-24 11:36:01 +0530

    c++

---
+++ multi_level_inheritance.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Parent class
+class MyClass {
+  public: 
+    void myFunction() {
+    cout << "SAME CONTANT IN PARANT CLASS." ;
+    }
+};
+
+// Child class
+class MyChild: public MyClass {
+};
+
+// Grandchild class 
+class MyGrandChild: public MyChild {
+};
+
+int main() {
+  MyGrandChild myObj;
+  myObj.myFunction();
+  return 0;
+}
+
---
+++ multiple_inheritance.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class MyClass {
+  public:
+    void myFunction() {
+      cout << "THIS IS SAME CLASS AS PARENT CLASS.\n" ;
+    }
+};
+
+// Another base class
+class MyOtherClass {
+  public:
+    void myOtherFunction() {
+      cout << "THIS IS ANOTHER CLASS.\n" ;
+    }
+};
+
+// Derived class
+class MyChildClass: public MyClass, public MyOtherClass {
+};
+
+int main() {
+  MyChildClass myObj;
+  myObj.myFunction();
+  myObj.myOtherFunction();
+  return 0;
+}
+
